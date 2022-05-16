d393aa8204eaf6a1c258aff869376edc097604c5
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-05-16 23:30:07 +0700

    SQLite connected, table create function is implemented

--- main.py
+++ main.py
@@ -1,68 +1,41 @@
 from concurrent.futures import ThreadPoolExecutor
-import threading
-import time
+from threading import Thread
+from time import sleep
 import sqlite3 as db
 
 
-class Source1:
-
-    def __init__(self):
-        self.lock = threading.Lock()
-
-    def acquire(self, name):
-        print(f'{name} is acquiring the source1')
-        if not self.lock.locked():
-            self.lock.acquire()
-            print(f'The source1 is granted to {name}')
-        else:
-            print(f'Ooops, {name} is already locked by another thread')
-            self.lock.acquire()
-
-    def release(self, name):
-        self.lock.release()
-        print(f'{name} released the source1')
-
-
-class Source2:
-
-    def __init__(self):
-        self.lock = threading.Lock()
-
-    def acquire(self, name):
-        print('')
-        print(f'{name} is acquiring the source2')
-        if not self.lock.locked():
-            self.lock.acquire()
-            print(f'The source2 is granted to {name}')
-            print('')
-        else:
-            print(f'Ooops, {name} is already locked by another thread')
-            self.lock.acquire()
-
-    def release(self, name):
-        self.lock.release()
-        print(f'{name} released the source2')
-
-
-def process(name, source1, source2):
-    source1.acquire(name)
-    time.sleep(2)
-    source2.acquire(name)
-    time.sleep(2)
-    print('now release all the sources')
-    source1.release(name)
-    source2.release(name)
-
-
 def create_table():
-    sql="""DROP TABLE"""
+    sql = """DROP TABLE IF EXISTS player"""
+    cursor.execute(sql)
+    data_base.commit()
+    sql = """CREATE TABLE player(
+            player_id INTEGER PRIMARY KEY,
+            player_name TEXT,
+            player_points INTEGER)"""
+    cursor.execute(sql)
+    data_base.commit()
+    sql = """INSERT INTO player(player_name, player_points)
+            VALUES
+                    ('BigKing', 120),
+                    ('Eagle', 10),
+                    ('Commandor', 210);"""
+    cursor.execute(sql)
+    data_base.commit()
+    sql = """SELECT * FROM player"""
+    cursor.execute(sql)
+    data_base.commit()
+    array = cursor.fetchall()
+    for arr in array:
+        print(arr)
 
+def get_source():
+    pass
 
 if __name__ == '__main__':
     try:
         data_base = db.connect('Thread_lock.db')
         cursor = data_base.cursor()
-
+        create_table()
 
     except db.Error as error:
         print("Data Base Connection Error")
