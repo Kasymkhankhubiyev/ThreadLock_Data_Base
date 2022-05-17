d9d06266072d55b848ed07cafaccb4e1b06bf350
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-05-17 07:53:43 +0700

    PostgreSQL connected!!!!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from concurrent.futures import ThreadPoolExecutor
 from threading import Thread
 from time import sleep
 import sqlite3 as db
+import psycopg2
+from config import host, user, password, db_name
 
 
 def create_table():
@@ -28,14 +30,61 @@ def create_table():
     for arr in array:
         print(arr)
 
-def get_source():
-    pass
+
+def get_source(number):
+    print(f'Starting getting source {number}')
+    sql = """SELECT player_points FROM player WHERE player_id = ?"""
+    cursor.execute(sql, [number])
+    data_base.commit()
+    print(f'Source {number} is got')
+
+
+def update_points(number):
+    print(f'Statring changing points for {number}')
+    sql = """UPDATE player SET player_points = player_points - 2 WHERE player_id = ?"""
+    cursor.execute(sql, [number])
+    data_base.commit()
+    print(f'Points for source {number} is changed')
+
+
+
+def thread_func():
+    #data_base1 = psycopg2.connect(host='localhost', database='Thread_lock.db', user='postgres', password="12345")
+    data_base1 = db.connect('Thread_lock.db')
+
+    cursor1 = data_base1.cursor()
+    number = 1
+    print(f'Starting getting source {number}')
+    sql = """SELECT player_points FROM player WHERE player_id = ?"""
+    cursor1.execute(sql, [number])
+    data_base1.commit()
+    print(f'Source {number} is got')
+    sleep(1)
+    print(f'Statring changing points for {number+1}')
+    sql = """UPDATE player SET player_points = player_points - 2 WHERE player_id = ?"""
+    cursor1.execute(sql, [number+1])
+    data_base1.commit()
+    print(f'Points for source {number+1} is changed')
+    data_base1.close()
+
 
 if __name__ == '__main__':
     try:
+        # data_base = psycopg2.connect(host='localhost', database='Thread_lock.db', port='5432')
+        data_base = psycopg2.connect(host=host, user=user, password=password, database=db_name)
         data_base = db.connect('Thread_lock.db')
+        print("data base opened successfuly")
         cursor = data_base.cursor()
         create_table()
+
+        th = Thread(target=thread_func)
+        th.start()
+
+        get_source(2)
+        sleep(1)
+        update_points(1)
+
+
 
     except db.Error as error:
         print("Data Base Connection Error")
