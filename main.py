197254f50a687021bc557e4bde8346956b1c9542
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-05-18 19:56:35 +0700

    Dead Lock modelling with transactions. Unfortunately it does not work.

--- main.py
+++ main.py
@@ -33,38 +33,57 @@ def create_table():
 
 def get_source(number):
     print(f'Starting getting source {number}')
-    sql = """SELECT player_points FROM player WHERE player_id = ?"""
-    cursor.execute(sql, [number])
+    sql = """SELECT player_points FROM player WHERE player_id = 2 FOR UPDATE"""
+    cursor.execute(sql)
     data_base.commit()
     print(f'Source {number} is got')
 
 
 def update_points(number):
     print(f'Statring changing points for {number}')
-    sql = """UPDATE player SET player_points = player_points - 2 WHERE player_id = ?"""
-    cursor.execute(sql, [number])
+    sql = """UPDATE player SET player_points = player_points - 2 WHERE player_id = 1"""
+    cursor.execute(sql)
     data_base.commit()
     print(f'Points for source {number} is changed')
 
 
 
 def thread_func():
-    #data_base1 = psycopg2.connect(host='localhost', database='Thread_lock.db', user='postgres', password="12345")
-    data_base1 = db.connect('Thread_lock.db')
+    data_base1 = psycopg2.connect(host=host, user=user, password=password, database=db_name)
 
     cursor1 = data_base1.cursor()
     number = 1
-    print(f'Starting getting source {number}')
-    sql = """SELECT player_points FROM player WHERE player_id = ?"""
-    cursor1.execute(sql, [number])
+    print(f'The second thread started')
+    sql = """begin;
+            update player
+            set player_points = player_points + 2
+            where player_id = 2;
+            select pg_sleep(10);
+            select player_points
+            from player
+            where player_id = 1
+            for update;
+            select * from player where player_id = 2;
+            commit;"""
+    cursor1.execute(sql)
     data_base1.commit()
-    print(f'Source {number} is got')
-    sleep(1)
-    print(f'Statring changing points for {number+1}')
-    sql = """UPDATE player SET player_points = player_points - 2 WHERE player_id = ?"""
-    cursor1.execute(sql, [number+1])
-    data_base1.commit()
-    print(f'Points for source {number+1} is changed')
+    cursor1.close()
+    # array = cursor1.fetchall()
+    print(f'The second thread finished')
+    # for arr in array:
+    #     print(arr)
+
+    #print(f'Starting getting source {number}')
+    # sql = """SELECT player_points FROM player WHERE player_id = 1 FOR UPDATE"""
+    # cursor1.execute(sql)
+    # data_base1.commit()
+    # print(f'Source {number} is got')
+    # sleep(1000)
+    # print(f'Statring changing points for {number+1}')
+    # sql = """UPDATE player SET player_points = player_points - 2 WHERE player_id = 2"""
+    # cursor1.execute(sql)
+    # data_base1.commit()
+    # print(f'Points for source {number+1} is changed')
     data_base1.close()
 
 
@@ -72,17 +91,34 @@ if __name__ == '__main__':
     try:
         # data_base = psycopg2.connect(host='localhost', database='Thread_lock.db', port='5432')
         data_base = psycopg2.connect(host=host, user=user, password=password, database=db_name)
-        data_base = db.connect('Thread_lock.db')
         print("data base opened successfuly")
         cursor = data_base.cursor()
-        create_table()
 
         th = Thread(target=thread_func)
         th.start()
 
-        get_source(2)
-        sleep(1)
-        update_points(1)
+        print('The main thread started')
+        sql = """begin;
+                    select player_points
+                    from player
+                    where player_id = 2
+                    for update;
+                    select pg_sleep(10);
+                    update player
+                    set player_points = player_points + 2
+                    where player_id = 1;
+                    select * from player where player_id = 1;
+                    commit;"""
+        cursor.execute(sql)
+        data_base.commit()
+        print('The main thread finished')
+        # array = cursor.fetchall()
+        # for arr in array:
+        #     print(arr)
+
+        # get_source(2)
+        # sleep(1000)
+        # update_points(1)
 
 
 
@@ -91,6 +127,7 @@ if __name__ == '__main__':
     finally:
         if data_base:
             data_base.close()
+            cursor.close()
             print('Connection with SQL is closed')
 
     # source1 = Source1()
